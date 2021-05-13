f4214934eaba60084c8f1c159555799027d890a3
차승훈 <chachasmini@chaseunghun-ui-Macmini.local>
2021-05-13 23:18:18 +0900

    20210513_#2.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Movie({ title }) {
+  return <h1>This is {title} !</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+    <h1>Hello</h1>
+    <Movie title = "Minari" /> 
+    <Movie title = "Avengers" /> 
+    <Movie title = "Iron man" /> 
+    <Movie title = "Spider man" /> 
+  </div>);
 }
 
 export default App;
+ 
